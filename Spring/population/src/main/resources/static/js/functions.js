e1b3fe7bd4c0035de3eb8d70a394ad9dbd987c0c
yu <ysu96@naver.com>
2021-05-11 19:33:17 +0900

    board change

--- Spring/population/src/main/resources/static/js/functions.js
+++ Spring/population/src/main/resources/static/js/functions.js
@@ -17,68 +17,77 @@ function createObject(setting){
   let object;
 
 
+//
+//  if(setting.shape == "fas fa-video"){
+//    setting.shape = "fas fa-video fa-3x"
+//    object = document.createElement("i");
+//  }
+//  else{
+//    object = document.createElement("div");
+//  }
 
-  if(setting.shape == "fas fa-video"){
-    setting.shape = "fas fa-video fa-3x"
-    object = document.createElement("i");
-  }
-  else{
-    object = document.createElement("div");
-  }
+   var rect3 = new Konva.Rect({
+          x: 50,
+          y: 120,
+          width: 100,
+          height: 100,
+          fill: 'blue',
+          cornerRadius: [0, 10, 20, 30],
+        });
+   canvas.add(rect3);
 
-
-
-  if( setting.shape === "text" ){
-    object = document.createElement("input");
-    object.type = "text";
-  }
-
-  // 도형 모양 설정
-  if( setting.shape ){
-    classList.push(setting.shape);
-  }
-
-  // 배경 색상 설정
-  if( setting.bg_color ){
-    classList.push(setting.bg_color);
-  }
-
-  // 폰트 색상 설정
-  if( setting.font_color ){
-    classList.push(setting.font_color);
-  }
-
-  // 우선순위 설정
-  const priority = getMaxAttr(objects, "priority", 0);
-  object.setAttribute("priority", priority);
-  object.style["z-index"] = priority;
-
-
-
-  // 도형 사이즈 설정
-  if(setting.shape == "fas fa-video fa-3x"){
-    object.style.width = "30px";
-    object.style.height = "30px";
-  }
-  else if( setting.width && setting.height ){
-    object.style.width = setting.width+"px";
-    object.style.height = setting.height+"px";
-  }
-
-
-  
-  // 폰트 사이즈 설정
-  if( setting.font_size ){
-    object.style["font-size"] = setting.font_size+"px";
-  }
-
-  // 투명도 설정
-  if( setting.opacity ){
-    object.style["opacity"] = setting.opacity+"%";
-  }
-
-  // class 설정
-  object.className = classList.join(" ");
+//
+//  if( setting.shape === "text" ){
+//    object = document.createElement("input");
+//    object.type = "text";
+//  }
+//
+//  // 도형 모양 설정
+//  if( setting.shape ){
+//    classList.push(setting.shape);
+//  }
+//
+//  // 배경 색상 설정
+//  if( setting.bg_color ){
+//    classList.push(setting.bg_color);
+//  }
+//
+//  // 폰트 색상 설정
+//  if( setting.font_color ){
+//    classList.push(setting.font_color);
+//  }
+//
+//  // 우선순위 설정
+//  const priority = getMaxAttr(objects, "priority", 0);
+//  object.setAttribute("priority", priority);
+//  object.style["z-index"] = priority;
+//
+//
+//
+//  // 도형 사이즈 설정
+//  if(setting.shape == "fas fa-video fa-3x"){
+//    object.style.width = "30px";
+//    object.style.height = "30px";
+//  }
+//  else if( setting.width && setting.height ){
+//    object.style.width = setting.width+"px";
+//    object.style.height = setting.height+"px";
+//  }
+//
+//
+//
+//  // 폰트 사이즈 설정
+//  if( setting.font_size ){
+//    object.style["font-size"] = setting.font_size+"px";
+//  }
+//
+//  // 투명도 설정
+//  if( setting.opacity ){
+//    object.style["opacity"] = setting.opacity+"%";
+//  }
+//
+//  // class 설정
+//  object.className = classList.join(" ");
 
   // 이벤트 바인딩
   object.addEventListener("mousedown", handleObjectHold);
