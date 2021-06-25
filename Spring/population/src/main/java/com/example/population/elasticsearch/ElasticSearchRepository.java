dc61ccda5d751cb12d4acf5fd2ee7c2d07a8af09
Kyeong-Won <kwkang98@gmail.com>
2021-06-25 18:45:57 +0900

    rename

--- Spring/population/src/main/java/com/example/population/elasticsearch/ElasticSearchRepository.java
+++ Spring/population/src/main/java/com/example/population/elasticsearch/ElasticSearchRepository.java
@@ -1,8 +1,10 @@
 package com.example.population.repository;
 
 import com.example.population.domain.Camera;
+import com.example.population.domain.Member;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface ElasticSearchRepository extends ElasticsearchRepository<Camera, Long> {
+import java.util.Optional;
 
+public interface ElasticSearchRepository extends ElasticsearchRepository<Camera, Long> {
 }
